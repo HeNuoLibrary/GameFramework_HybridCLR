a7681eaf5aacb48e104eb5d45bbe130a64f52c83
HeNuoScott <1073498648@qq.com>
2022-11-14 10:41:05 +0800

    HybridCLR 升级到0.10.4

--- Assets/GameScripts/GameMain/HybridCLR/HuatuoComponent.cs
+++ Assets/GameScripts/GameMain/HybridCLR/HuatuoComponent.cs
@@ -78,6 +78,34 @@ namespace GameFrame.Main
             Log.Error("Can not load {0} from '{1}' with error message '{2}'.", assetName, "AOTAsset", errorMessage);
         }
 
+        ///// <summary>
+        ///// 为aot assembly加载原始metadata， 这个代码放aot或者热更新都行。
+        ///// 一旦加载后，如果AOT泛型函数对应native实现不存在，则自动替换为解释模式执行
+        ///// </summary>
+        //private static void LoadMetadataForAOTAssemblies()
+        //{
+        //    List<string> AOTMetaAssemblyNames = new List<string>()
+        //    {
+        //        "mscorlib.dll",
+        //        "System.dll",
+        //        "System.Core.dll",
+        //    };
+        //    // 可以加载任意aot assembly的对应的dll。但要求dll必须与unity build过程中生成的裁剪后的dll一致，而不能直接使用原始dll。
+        //    // 我们在BuildProcessors里添加了处理代码，这些裁剪后的dll在打包时自动被复制到 {项目目录}/HybridCLRData/AssembliesPostIl2CppStrip/{Target} 目录。
+
+        //    /// 注意，补充元数据是给AOT dll补充元数据，而不是给热更新dll补充元数据。
+        //    /// 热更新dll不缺元数据，不需要补充，如果调用LoadMetadataForAOTAssembly会返回错误
+        //    /// 
+        //    HomologousImageMode mode = HomologousImageMode.SuperSet;
+        //    foreach (var aotDllName in AOTMetaAssemblyNames)
+        //    {
+        //        byte[] dllBytes = GetAssetData(aotDllName);
+        //        // 加载assembly对应的dll，会自动为它hook。一旦aot泛型函数的native函数不存在，用解释器版本代码
+        //        LoadImageErrorCode err = RuntimeApi.LoadMetadataForAOTAssembly(dllBytes, mode);
+        //        Debug.Log($"LoadMetadataForAOTAssembly:{aotDllName}. mode:{mode} ret:{err}");
+        //    }
+        //}
+
         /// <summary>
         /// 为aot assembly加载原始metadata， 这个代码放aot或者热更新都行。
         /// 一旦加载后，如果AOT泛型函数对应native实现不存在，则自动替换为解释模式执行
@@ -87,14 +115,14 @@ namespace GameFrame.Main
             // 可以加载任意aot assembly的对应的dll。但要求dll必须与unity build过程中生成的裁剪后的dll一致，而不能直接使用原始dll。
             // 我们在BuildProcessors里添加了处理代码，这些裁剪后的dll在打包时自动被复制到 {项目目录}/HybridCLRData/AssembliesPostIl2CppStrip/{Target} 目录。
 
-            /// 注意，补充元数据是给AOT dll补充元数据，而不是给热更新dll补充元数据。
-            /// 热更新dll不缺元数据，不需要补充，如果调用LoadMetadataForAOTAssembly会返回错误
-            /// 
+            // 注意，补充元数据是给AOT dll补充元数据，而不是给热更新dll补充元数据。
+            // 热更新dll不缺元数据，不需要补充，如果调用LoadMetadataForAOTAssembly会返回错误 
+            HomologousImageMode mode = HomologousImageMode.SuperSet;
             foreach (var aotDllName in assemblyManifest.AOTMetadataDlls)
             {
                 byte[] dllBytes = m_loadedHotifx[aotDllName];
                 // 加载assembly对应的dll，会自动为它hook。一旦aot泛型函数的native函数不存在，用解释器版本代码
-                LoadImageErrorCode err = RuntimeApi.LoadMetadataForAOTAssembly(dllBytes);
+                LoadImageErrorCode err = RuntimeApi.LoadMetadataForAOTAssembly(dllBytes, mode);
                 Debug.Log($"LoadMetadataForAOTAssembly:{aotDllName}. ret:{err}");
             }
         }
